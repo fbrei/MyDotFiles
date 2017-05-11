940ea3c0b623181e396d80bdfa0a50c93788b9c5
Felix Brei <felix.brei@posteo.de>
2017-05-11 17:28:07 +0200

    Some customizations of the tmux config

--- tmux_work.sh
+++ tmux_work.sh
@@ -7,15 +7,10 @@ $TMUX has-session -t $SESSIONNAME &> /dev/null
 
 if [ $? != 0 ];
 then
-    echo "TEST"
     $TMUX new-session -d -s $SESSIONNAME
 
     $TMUX new-window -t $SESSIONNAME:0
-    $TMUX rename-window -t $SESSIONNAME:0 Splitscreen
-    $TMUX split-window -h -t $SESSIONNAME:0
-
-    $TMUX new-window -t $SESSIONNAME:1
-    $TMUX rename-window -t $SESSIONNAME:1 Fullscreen
+    $TMUX rename-window -t $SESSIONNAME:0 Main
 fi
 
-$TMUX attach-session -t $SESSIONNAME:0
+$TMUX attach-session -t $SESSIONNAME
